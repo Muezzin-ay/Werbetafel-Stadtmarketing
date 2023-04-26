db88871be8277817d9692e0edea079c253b162dc
Robin Hart <hartrobin05@gmail.com>
2023-04-26 10:44:46 +0200

    created logic for file upload in frontend

---
+++ public/components/upload.js
@@ -0,0 +1,59 @@
+
+// FROM https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+})
+  
+
+function preventDefaults (e) {
+    e.preventDefault()
+    e.stopPropagation()
+}
+
+;['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false)
+})
+
+function highlight(e) {
+    dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+    dropArea.classList.remove('highlight')
+}
+
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+  let dt = e.dataTransfer
+  let files = dt.files
+
+  handleFiles(files)
+}
+
+function handleFiles(files) {
+    ([...files]).forEach(uploadFile)
+}
+
+
+function uploadFile(file) {
+    console.log("good!");
+    /**
+    let url = 'YOUR URL HERE'
+    let formData = new FormData()
+  
+    formData.append('file', file)
+  
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    })
+    .then(() => { })
+    .catch(() => { })
+    **/
+}
