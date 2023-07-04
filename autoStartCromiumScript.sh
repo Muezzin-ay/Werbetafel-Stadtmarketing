0ae5f97d15183d3bfb78527185826a07217f2c27
Robin Hart <hartrobin05@gmail.com>
2023-07-04 19:54:58 +0200

    s

--- autoStartCromiumScript.sh
+++ autoStartCromiumScript.sh
@@ -3,5 +3,5 @@ echo "Starting chromium in 5 seconds..."
 sleep 5
 while :
     do
-        chromium-browser DISPLAY=:0.0 xeyes --noerrdialogs --kiosk --no-sandbox http://localhost:8084/show
+        chromium-browser --noerrdialogs --kiosk --no-sandbox http://localhost:8084/show
 done
