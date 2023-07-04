91118d58804bb05ce28d70857003b32199a46ced
Robin Hart <hartrobin05@gmail.com>
2023-07-04 19:50:30 +0200

    changed startup file

--- autoStartCromiumScript.sh
+++ autoStartCromiumScript.sh
@@ -3,5 +3,5 @@ echo "Starting chromium in 5 seconds..."
 sleep 5
 while :
     do
-        chromium-browser --noerrdialogs --kiosk http://localhost:8084/show
+        chromium-browser --noerrdialogs --kiosk --no-sandbox http://localhost:8084/show
 done
