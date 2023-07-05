c51cdb087a683e579e4adbcd4640b694736f9184
Robin Hart <hartrobin05@gmail.com>
2023-07-05 13:01:38 +0200

    changed autostart File

---
+++ autoStartChromiumScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Starting chromium in 5 seconds..."
+sleep 5
+while :
+    do
+        chromium-browser --noerrdialogs --kiosk http://localhost:8084/show
+done
+
+## Information and Todo ##
+# --no-sandbox
+# write to 'rc.local' -file at '/etc/rc.local' :
+# sh [Path to Start File]/autoStartChromiumScript.sh
--- autoStartCromiumScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting chromium in 5 seconds..."
-sleep 5
-while :
-    do
-        chromium-browser --noerrdialogs --kiosk --no-sandbox http://localhost:8084/show
-done
